64a21464bd99c2dd0727cc4209f6f3a99534d1b1
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-25 00:37:37 +0700

    Update BIẾN ĐỔI DÃY SỐ.py

--- BIẾN ĐỔI DÃY SỐ.py
+++ BIẾN ĐỔI DÃY SỐ.py
@@ -1,41 +1,34 @@
-# line_1,line_2= input(), input()
 line_1 = input()
-c = 0
+c,n = 0,0
 
 arr_1 = line_1.split()
-# arr_2 = line_2.split()
 q = []
 tmp = len(arr_1)
 for i in range(0, tmp):
     arr_1[i] = int(arr_1[i])
-    # arr_2[i] = int(arr_2[i])
-
-while( len(set(arr_1)) > 1 ):
-    c += 1
-    for i in range( 0,tmp ):
-        if i == tmp - 1:
-            q.append( abs(arr_1[i] - arr_1[0]) )
-        else:
-            q.append( abs(arr_1[i] - arr_1[i+1]) )
-
-    for i in range( 0,tmp ):
-        arr_1[i] = q.pop(0)
-print(c)
+for i in set(arr_1): #do truong hop dac biet nhu 1 1 1 1  hay 2 2 2 2
+    n = i            #nen phai check
 
 
-# c=0
-# while( len(set(arr_2)) > 1 ):
-#     c += 1
-#     for i in range( 0,tmp ):
-#         if i == tmp - 1:
-#             q.append( abs(arr_2[i] - arr_2[0]) )
-#
-#         else:
-#             q.append( abs(arr_2[i] - arr_2[i+1]) )
-#
-#     for i in range( 0,tmp ):
-#         arr_2[i] = q.pop(0)
-#
-#
-# print(c)
+while(n != 0 ): #neu nhap vao toan khong thi thoat vong lap
+    c=0
+    while( len(set(arr_1)) > 1 ):
+        c += 1
+        for i in range( 0,tmp ):
+            if i == tmp - 1:
+                q.append( abs(arr_1[i] - arr_1[0]) )
+            else:
+                q.append( abs(arr_1[i] - arr_1[i+1]) )
+
+        for i in range( 0,tmp ):
+            arr_1[i] = q.pop(0)
+    print(c)
+
+    line_1 = input()                #nhap case moi
+    arr_1 = line_1.split()
+    tmp = len(arr_1)
+    for i in range(0, tmp):
+        arr_1[i] = int(arr_1[i])
+    for i in set(arr_1):
+        n = i
 
