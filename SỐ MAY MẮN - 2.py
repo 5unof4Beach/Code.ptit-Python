3440b814b44e96372fb64db0586ba10e01958ebf
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-25 13:27:00 +0700

    Create SỐ MAY MẮN - 2.py

---
+++ SỐ MAY MẮN - 2.py
@@ -0,0 +1,13 @@
+test = int(input())
+while test > 0:
+    test -= 1
+    n = input()
+    check = 1
+    for i in n:
+        if i != '4' and i != '7':
+            check = 0
+            break
+    if check == 1:
+        print("YES")
+    else:
+        print("NO")
