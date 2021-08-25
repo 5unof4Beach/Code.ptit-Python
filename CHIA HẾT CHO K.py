ef329b37473ca39abafb8b0532b71da18df4a880
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-25 08:49:21 +0700

    test git

--- CHIA HẾT CHO K.py
+++ CHIA HẾT CHO K.py
@@ -7,12 +7,15 @@ for i in range(0,len(arr)):
 c=1
 temp = arr[1]
 res = []
+
 while(temp <= arr[2]):
+
     c += 1
     temp = arr[1] * c
-    if(arr[1]*c > arr[2]): break
-    if (arr[1] * c - arr[0] > 0):
+    if( arr[1]*c > arr[2] ): break
+    if ( arr[1]*c - arr[0] > 0 ):
         res.append(arr[1]*c - arr[0] )
+
 if(len(res)==0):
     print(-1)
 else:
