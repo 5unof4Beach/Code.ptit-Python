8ad284472a506ecb40bc9486b049d649aecc06cd
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-25 13:27:15 +0700

    a

--- DÃY SỐ PHÙ HỢP.py
+++ DÃY SỐ PHÙ HỢP.py
@@ -5,16 +5,42 @@ while(T):
     line_1, line_2 = input(), input()
     arr_1, arr_2 = line_1.split(), line_2.split()
     flag = 1
-    arr_1.sort()
-    arr_2.sort()
+    pos = []
+    c=0
+    for i in range(0,n):
+        if(arr_1[i] > arr_2[i]):
+            temp = i
+            break
+    for i in range(0, n):
+        if((arr_1[i] <= arr_2[i])):
+            #swap
+            num = arr_1[i]
+            arr_1[i] = arr_1[temp]
+            arr_1[temp] = num
+            break
+
+    for i in range(0, n):
+        if ((arr_1[i] > arr_2[i])):
+            temp = i
+
+    for i in range(0, n):
+        if(i == temp ):
+            continue
+        else:
+            if(arr_2[i] >= arr_1[temp] and arr_2[temp] >= arr_1[i]):
+                num = arr_2[i]
+                arr_2[i] = arr_2[temp]
+                arr_2[temp] = num
+    print(arr_1)
+    print(arr_2)
+    
     for i in range(0,n):
         if(arr_1[i] > arr_2[i]):
             print("NO")
             flag = 0
             break
-    if(flag):
+    if flag:
         print("YES")
-
     T -= 1
 
 
---
+++ SỐ THUẬN NGHỊCH CHẴN.py
@@ -0,0 +1,5 @@
+n = int(input())
+temp = str(n)
+no_of_digit = len(temp) #dem so cac chu so
+list = [2,4,6,8,22,24,26,28,42,44,46,48,62,64,66,68,82,84,86,88,222,224,226,228]
+
