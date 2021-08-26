0fb7b2618faf6169051a6198918a7aa5eef44767
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-26 23:43:27 +0700

    WA

--- CHIA DƯ CHO 42.py
+++ CHIA DƯ CHO 42.py
@@ -3,7 +3,18 @@ line = line.split()
 arr = []
 for i in line:
     arr.append( int(i) % 42 )
+c=0
+hash = dict()
+for i in arr:
+    if i in hash:
+        hash[i] +=1
+    else:
+        hash[i] = 1
 
-arr = set(arr)
+for key in hash:
+    c += 1
 print("")
-print(len(arr))
+print(c)
+# arr = set(arr)
+# print("")
+# print(len(arr))
