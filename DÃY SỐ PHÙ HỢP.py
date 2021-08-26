64ddbcf31e933171a58c77713e1f751c5f6d040a
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-26 23:19:46 +0700

    da ac

--- DÃY SỐ PHÙ HỢP.py
+++ DÃY SỐ PHÙ HỢP.py
@@ -1,57 +1,23 @@
 T = int(input())
-while(T):
-
+while (T):
+    T -= 1
     n = int(input())
     line_1, line_2 = input(), input()
     arr_1, arr_2 = line_1.split(), line_2.split()
-    flag = 1
-    pos = []
-    c=0
+    flag = 11
     for i in range(0,n):
-        if(arr_1[i] > arr_2[i]):
-            temp = i
-            break
-    for i in range(0, n):
-        if((arr_1[i] <= arr_2[i])):
-            #swap
-            num = arr_1[i]
-            arr_1[i] = arr_1[temp]
-            arr_1[temp] = num
-            break
-
-    for i in range(0, n):
-        if ((arr_1[i] > arr_2[i])):
-            temp = i
-
-    for i in range(0, n):
-        if(i == temp ):
-            continue
-        else:
-            if(arr_2[i] >= arr_1[temp] and arr_2[temp] >= arr_1[i]):
-                num = arr_2[i]
-                arr_2[i] = arr_2[temp]
-                arr_2[temp] = num
-    print(arr_1)
-    print(arr_2)
-    
-    for i in range(0,n):
-        if(arr_1[i] > arr_2[i]):
+        arr_1[i] = int(arr_1[i])
+        arr_2[i] = int(arr_2[i])
+    arr_1.sort()
+    arr_2.sort()
+    for i in range(0, n - 1):
+        if (arr_1[i] > arr_2[i]):
             print("NO")
             flag = 0
             break
-    if flag:
+    if (flag):
         print("YES")
-    T -= 1
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # for i in range (0,n):
+#     arr_1[i] = int(arr_1[i])
+#     arr_2[i] = int(arr_2[i])
