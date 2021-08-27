014d6819a79393b9019dbdb8beeb1308ba3bf835
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-27 23:31:40 +0700

    ac

--- TỔNG CHỮ SỐ.py
+++ TỔNG CHỮ SỐ.py
@@ -1,16 +1,26 @@
 n = input()
-if(n[0] == '-'):
-    n = n[1:len(n)]
-
+flag = 0
+# if(n[0] == '-'): # loai bo dau tru
+#     n = n[1:len(n)]
+#     flag = 1
 # print(n)
 def sum(n):
     temp = 0
     for i in n:
-        temp += int(i)
-    return str(temp)
-c=0
-while(len(n) != 1):
-    c +=1
-    n = sum(n)
+        if i == '-':
+            temp += ord('-') - ord('0')
+        else:
+            temp += int(i)
 
-print(c)
+    return str(temp)
+
+if(len(n) == 1):
+    print(1)
+else:
+    c=0
+    while(len(n) != 1):
+        c +=1
+        # print(sum(n))
+        n = sum(n)
+
+    print(c)
