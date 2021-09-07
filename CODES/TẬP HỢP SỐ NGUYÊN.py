1771394ae0b6443337cbf2e4726edfc84635dff3
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-07 20:24:58 +0700

    ac

--- CODES/TẬP HỢP SỐ NGUYÊN.py
+++ CODES/TẬP HỢP SỐ NGUYÊN.py
@@ -1,14 +1,20 @@
 line = input()
-line1 = input()
-line2 = input()
+in1 = input()
+in2 = input()
 
-line1 = line1.split()
-for i in range(len(line1)):
-    line1[i] = int(line1[i])
-    
-line2 = line2.split()
-for i in range(len(line2)):
-    line2[i] = int(line2[i])
+in1 = in1.split()
+for i in range(len(in1)):
+    in1[i] = int(in1[i])
+
+in2 = in2.split()
+for i in range(len(in2)):
+    in2[i] = int(in2[i])
+line1 = []
+line2 = []
+
+for i in set(in1):line1.append(i)
+for i in set(in2):line2.append(i)
+
 line1.sort()
 line2.sort()
 
