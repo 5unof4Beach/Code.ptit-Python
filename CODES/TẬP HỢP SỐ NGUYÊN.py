d3fcb8d847cea08404b3ad5a272adaf4481ef0ee
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-07 20:16:58 +0700

    wa

--- CODES/TẬP HỢP SỐ NGUYÊN.py
+++ CODES/TẬP HỢP SỐ NGUYÊN.py
@@ -1,36 +1,27 @@
 line = input()
-line = line.split()
-n1 , n2 = int(line[0]),int(line[1])
 line1 = input()
 line2 = input()
+
 line1 = line1.split()
+for i in range(len(line1)):
+    line1[i] = int(line1[i])
+    
 line2 = line2.split()
+for i in range(len(line2)):
+    line2[i] = int(line2[i])
 line1.sort()
 line2.sort()
+
 arr = []
-for i in line1: arr.append(i)
-for i in line2: arr.append(i)
-
-hash = dict()
-
-for i in arr:
-    if i in hash:
-        hash[i] += 1
-    else:
-        hash[i] = 1
-arr.clear()
-
-for key in hash:
-    if hash[key] == 2:
-        print(key,"",end="")
-print("")
-
 for i in line1:
-    if hash[i] == 1:
-        print(i,"",end="")
-print("")
-
+    if i in line2:
+        print(i,end=" ")
+        arr.append(i)
+print()
+for i in line1:
+    if i not in arr:
+        print(i,end=" ")
+print()
 for i in line2:
-    if hash[i] == 1:
-        print(i,"",end="")
-print("")
+    if i not in arr:
+        print(i,end=" ")
