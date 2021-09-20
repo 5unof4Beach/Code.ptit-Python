7f8e028bd6937b5ca073d5ae764370c8976c92ce
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-20 19:55:40 +0700

    ac

--- CODES/main.py
+++ CODES/main.py
@@ -1,17 +1,18 @@
-print("Bai 11")
-line = "he did a a a nothing except camp out where he was told to go and he he father a few children"
-word = dict()
-line = line.split()
-for i in line:
-    if i in word:
-        word[i] += 1
-    else:
-        word[i] = 1
-res = sorted(word.items(), key = lambda x:x[1],reverse=True)
-for i in res:
-    if i[1]>3:
-        print(i)
+# print("Bai 5")
+# n  = int(input())
+# sum = 0
+# f1 = 0
+# f2 = 1
+# print(f1,f2,sep = " ",end=" ")
+# while sum<=30:
+#     print(sum,sep = " ",end=" ")
+#     sum = f1 +f2
+#     f1,f2 = f2,sum
 
-print("Bai 12")
-# res = sorted(word.values(), key = lambda x:x[1],reverse=True)
-print(res[0],res[-1])
+print("Bai 6")
+inp = '0100,0011,1010,1001,1100,1001'
+line = []
+line = [i for i in inp.split(",")]
+for i in line:
+    if int(i,2) % 10 == 0:
+        print(i)
